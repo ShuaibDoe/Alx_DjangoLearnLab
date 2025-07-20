44f84434bc39747e8de1979fed76dad48a846702
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-07-20 04:59:56 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library
 
-# Create your views here.
+# Class-based view for a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
